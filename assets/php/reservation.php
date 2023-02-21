cdbca4c3a8e384074ac0b290f41b8029671716d1
kasperaho <113963555+kasperaho@users.noreply.github.com>
2023-02-21 09:54:00 +0200

    varausta

---
+++ assets/php/reservation.php
@@ -0,0 +1,36 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try{
+    $yhteys=mysqli_connect("db", "pena", "kukkuu", "spawn");
+}
+catch(Exception $e){
+    header("Location:../html/yhteysvirhe.html");
+    exit;
+}
+
+//Luetaan lomakkeelta tulleet tiedot funktiolla $_POST
+//jos syötteet ovat olemassa
+$name=isset($_POST["name"]) ? $_POST["name"] : "";
+$email=isset($_POST["email"]) ? $_POST["email"] : "";
+$details=isset($_POST["details"]) ? $_POST["details"] :"";
+
+//Jos ei jompaa kumpaa tai kumpaakaan tietoa ole annettu
+//ohjataan pyyntö takaisin lomakkeelle
+if (empty($name) || empty($email)|| empty($details) ){
+    header("Location:../contactpage.html");
+    exit;
+}
+
+//Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
+//joihin laitetaan muuttujien arvoja
+$sql="insert into reservation (name, email, details) values(?, ?, ?)";
+
+//Valmistellaan sql-lause
+$stmt=mysqli_prepare($yhteys, $sql);
+//Sijoitetaan muuttujat oikeisiin paikkoihin
+mysqli_stmt_bind_param($stmt, 'sss', $name, $email, $details);
+//Suoritetaan sql-lause
+mysqli_stmt_execute($stmt);
+//Suljetaan tietokantayhteys
+mysqli_close($yhteys);
+?>
