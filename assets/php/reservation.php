998bdb6111db5bc4dec07d716a0420f459d3384b
kasperaho <113963555+kasperaho@users.noreply.github.com>
2023-02-21 10:09:24 +0200

    varausedit2

--- assets/php/reservation.php
+++ assets/php/reservation.php
@@ -10,25 +10,26 @@ catch(Exception $e){
 
 //Luetaan lomakkeelta tulleet tiedot funktiolla $_POST
 //jos syötteet ovat olemassa
+$date=isset($_POST["date"]) ? $_POST["date"] : "";
 $fname=isset($_POST["name"]) ? $_POST["name"] : "";
 $email=isset($_POST["email"]) ? $_POST["email"] : "";
 $details=isset($_POST["details"]) ? $_POST["details"] :"";
 
 //Jos ei jompaa kumpaa tai kumpaakaan tietoa ole annettu
 //ohjataan pyyntö takaisin lomakkeelle
-if (empty($fname) || empty($email)|| empty($details) ){
+if  (empty($date))||(empty($fname) || empty($email)|| empty($details) ){
     header("Location:../contactpage.html");
     exit;
 }
 
 //Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
 //joihin laitetaan muuttujien arvoja
-$sql="insert into reservation (name, email, details) values(?, ?, ?)";
+$sql="insert into reservation (date, name, email, details) values(?, ?, ?, ?)";
 
 //Valmistellaan sql-lause
 $stmt=mysqli_prepare($yhteys, $sql);
 //Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'sss', $fname, $email, $details);
+mysqli_stmt_bind_param($stmt, 'isss', $date, $fname, $email, $details);
 //Suoritetaan sql-lause
 mysqli_stmt_execute($stmt);
 //Suljetaan tietokantayhteys
